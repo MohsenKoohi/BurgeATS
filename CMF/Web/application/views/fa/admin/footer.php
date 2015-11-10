ce3b263716fa77ebc1e4e398984b6d687fa4205f
Mohsen Koohi <koohi@burge.ir>
2015-11-10 12:38:42 +0330

    footer

---
+++ CMF/Web/application/views/fa/admin/footer.php
@@ -0,0 +1,11 @@
+	</div>
+</div>
+
+<div class="footer">
+	<a href="http://burge.ir/products/affairs-tracking-system">
+		<img src="{images_url}/logo-text-en.png"/>
+	</a>
+</div>
+
+</body>
+</html>

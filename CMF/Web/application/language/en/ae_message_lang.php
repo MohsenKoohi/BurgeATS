6be3df25af74e7845f5b59de2105cf5e2bd555f5
Mohsen Koohi <koohi@burge.ir>
2016-04-28 16:42:22 +0430

    message admin eng lang

--- CMF/Web/application/language/en/ae_message_lang.php
+++ CMF/Web/application/language/en/ae_message_lang.php
@@ -11,7 +11,66 @@ $lang['verifier']="Verifier";
 $lang['supervisor']="Supervisor";
 
 $lang['user_access_set_successfully']="User access set successfully.";
-$lang['']="";
-$lang['']="";
-$lang['']="";
-$lang['']="";
+
+$lang['messages']="Messages";
+$lang['sender']="Sender";
+$lang['receiver']="Receiver";
+$lang['me']="Me";
+$lang['customer']="Custoemr";
+$lang['department']="Department";
+
+$lang['sender_department']="Sender Department";
+$lang['sender_customer_name_or_id']="Id or Name of Sender ";
+$lang['sender_user_name_or_id']="Id or Name of Sender";
+
+$lang['receiver_department']="Receiver Department";
+$lang['receiver_customer_name_or_id']="Id or Name of Sender";
+$lang['receiver_user_name_or_id']="Id or Name of Sender";
+
+$lang['search_again']="Search";
+
+$lang['status']="Status";
+$lang['changing']="Changing";
+$lang['complete']="Complete";
+
+$lang['verification_status']="Verification Status (Customer to Customer)";
+$lang['verified']="Verified";
+$lang['not_verified']="Not Verified";
+
+$lang['start_date']="Start Date";
+$lang['end_date']="End Date";
+
+$lang['sender_from']="From";
+$lang['receiver_to']="To";
+$lang['subject']="Subject";
+$lang['content']="Content";
+$lang['verify']="Verify";
+$lang['from']="From";
+$lang['to']="To";
+$lang['results']="Results";
+$lang['page']="Page";
+$lang['total_results']="Total";
+$lang['are_you_sure_to_submit']="Are you sure?";
+$lang['verifications_saved_successfully']="Verification status of messages saved successfully.";
+
+$lang['message']="Message";
+$lang['date']="Date";
+$lang['number']="Number";
+$lang['content_of_last_message']="Last Content";
+$lang['time_of_last_activity']="Time of Last Activity";
+$lang['verification_status_of_last_message']="Status of Last Message";
+$lang['view_details']="View Details";
+
+$lang['response']="Response";
+$lang['language']="Language";
+$lang['response_content']="Content";
+$lang['comment']="Comment";
+$lang['send']="Send";
+$lang['active']="Active";
+$lang['inactive']="Inactive";
+$lang['type']="Sending Type";
+$lang['add_comment']="Add Comment";
+$lang['send_reply']="Send Reply";
+$lang['your_comment_added_successfully']="Your comment added successfully.";
+$lang['reply_or_comment']="Send Reply / Add Comment";
+$lang['your_reply_added_successfully']="Your reply added successfully.";

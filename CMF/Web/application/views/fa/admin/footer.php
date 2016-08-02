24529e00baab506c0404463298a255baea1c831f
Mohsen Koohi <koohi@burge.ir>
2016-08-02 23:07:50 +0430

    merge with CMF; style correction

--- CMF/Web/application/views/fa/admin/footer.php
+++ CMF/Web/application/views/fa/admin/footer.php
@@ -3,7 +3,7 @@
 </div>
 
 <div class="footer">
-	<a href="http://burge.ir/BurgeATS" target="_blank">
+	<a href="http://burge.ir/category-4/BurgeATS" target="_blank">
 		<img src="{images_url}/logo-text-en.png"  title="Burge ATS Homepage"/>
 	</a>
 </div>

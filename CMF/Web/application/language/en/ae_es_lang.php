c531e527840429d4423458fced01896a9cdaf5d9
Mohsen Koohi <koohi@burge.ir>
2017-09-03 21:39:09 +0200

    ES Manger > ENglish

---
+++ CMF/Web/application/language/en/ae_es_lang.php
@@ -0,0 +1,22 @@
+<?php 
+
+$lang['es']="Email and SMS";
+$lang['es_status_waiting']="Waiting";
+$lang['es_status_sent']="Sent";
+$lang['es_status_canceled']="Canceled";
+$lang['customer']="Customer";
+$lang['start_date']="Start date";
+$lang['end_date']="End date";
+$lang['status']="Status";
+$lang['type']="Type";
+$lang['search_again']="Search";
+$lang['results']="Results";
+$lang['to']="to";
+$lang['total_results']="Total";
+$lang['page']="Page";
+$lang['sms']="SMS";
+$lang['submit_time']="Submit time";
+$lang['last_try']="Last try";
+$lang['try_count']="Try count";
+$lang['module']="Module";
+$lang['keyword']="Keyword";
